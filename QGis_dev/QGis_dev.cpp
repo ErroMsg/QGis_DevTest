9e5b4fe403b681cda51ac32e39b03c9c1ad317cb
ErroMsg <gpy_1990@163.com>
2019-01-29 18:58:45 +0800

    ...

--- QGis_dev/QGis_dev.cpp
+++ QGis_dev/QGis_dev.cpp
@@ -1,19 +1,85 @@
 #include "QGis_dev.h"
+#include <QAction>
+#include <QDebug>
+#include <QFileDialog>
+#include <QMessageBox>
+#include "gui/qgsmapcanvas.h"
+#include "core/qgsvectorlayer.h"
+#include "core/qgsmaplayerregistry.h"
+#include "core/raster/qgsrasterlayer.h"
 
 QGis_dev::QGis_dev(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	mapCanvas = new QgsMapCanvas(); 
+	this->setCentralWidget(mapCanvas); 
+	mapCanvas->enableAntiAliasing(true); 
+	mapCanvas->setCanvasColor(QColor(255, 255, 255)); 
+	mapCanvas->setVisible(true);
+	
+	initUi();
 }
 
 void QGis_dev::initUi()
 {
+	QAction *pActAddVector = new QAction(this);
+	pActAddVector->setText("add vector layer");
+	ui.mainToolBar->addAction(pActAddVector);
+
+	QAction *pActAddRaster = new QAction(this);
+	pActAddRaster->setText("add raster layer");
+	ui.mainToolBar->addAction(pActAddRaster);
+
+	connect(pActAddVector, &QAction::triggered, this, &QGis_dev::displayVectorLayer);
+	connect(pActAddRaster, &QAction::triggered, this, &QGis_dev::displayRasterLayer);
 }
 
 void QGis_dev::displayVectorLayer()
 {
+	QString filename = QFileDialog::getOpenFileName(this, tr("open vector"), "", "*.shp"); 
+	QStringList temp = filename.split(QDir::separator()); 
+	QString basename = temp.at(temp.size() - 1); 
+	QgsVectorLayer *vecLayer = new QgsVectorLayer(filename, basename, "ogr", false); 
+	if (!vecLayer->isValid()) 
+	{ 
+		QMessageBox::critical(this, "error", "layer is invalid"); return; 
+	} 
+	QgsMapLayerRegistry::instance()->addMapLayer(vecLayer);
+
+	QList<QgsMapCanvasLayer> mapCanvasLayerSet;
+	QgsMapCanvasLayer layer(vecLayer);
+	mapCanvasLayerSet.append(layer);
+
+	mapCanvas->setExtent(vecLayer->extent());
+	mapCanvas->setLayerSet(mapCanvasLayerSet);
+	mapCanvas->setVisible(true);
+	mapCanvas->freeze(false);
+	mapCanvas->refresh();
 }
 
 void QGis_dev::displayRasterLayer()
 {
+	QString filename = QFileDialog::getOpenFileName(this, tr("open vector"), "", "*.tif"); 
+	QStringList temp = filename.split(QDir::separator()); 
+	QString basename = temp.at(temp.size() - 1); 
+	QgsRasterLayer* rasterLayer = new QgsRasterLayer(filename, basename, "gdal", false);
+	if (!rasterLayer->isValid()) 
+	{ 
+		QMessageBox::critical(this, "error", "layer is invalid"); 
+		return; 
+	} 
+	QgsMapLayerRegistry::instance()->addMapLayer(rasterLayer); 
+	
+	QList<QgsMapCanvasLayer> mapCanvasLayerSet;
+	QgsMapCanvasLayer layer(rasterLayer);
+	mapCanvasLayerSet.append(layer);
+
+	mapCanvas->setExtent(rasterLayer->extent()); 
+	mapCanvas->setLayerSet(mapCanvasLayerSet);
+	mapCanvas->setVisible(true); 
+	mapCanvas->freeze(false); 
+	mapCanvas->refresh();
+
 }
--- QGis_dev/QGis_dev.h
+++ QGis_dev/QGis_dev.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QList>
+#include "gui/qgsmapcanvas.h"
 #include "ui_QGis_dev.h"
 
+
+class QgsMapCanvas;
+//class QgsMapCanvasLayer;
 class QGis_dev : public QMainWindow
 {
 	Q_OBJECT
@@ -18,4 +23,7 @@ public slots:
 
 private:
 	Ui::QGis_devClass ui;
+
+	QgsMapCanvas *mapCanvas; // 地图画布
+	 // 地图画布所用的图层集合
 };
